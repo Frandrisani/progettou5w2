6bfaa39e2ad337ef1d4f064f3eb99aacd8c4181f
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-12 16:01:05 +0100

    Adding @Service in the classes EmployeeService and DeviceService

--- progettou5w2/src/main/java/francescoandrisani/progettou5w2/services/DipendenteService.java
+++ progettou5w2/src/main/java/francescoandrisani/progettou5w2/services/DipendenteService.java
@@ -13,10 +13,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Service
 public class DipendenteService {
 
     @Autowired
