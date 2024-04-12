fc63102413527a9000c3d612c82b44d87e115a19
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-12 10:42:54 +0100

    Initial commit

---
+++ progettou5w2/src/main/java/francescoandrisani/progettou5w2/Progettou5w2Application.java
@@ -0,0 +1,13 @@
+package francescoandrisani.progettou5w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettou5w2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettou5w2Application.class, args);
+	}
+
+}
---
+++ progettou5w2/src/test/java/francescoandrisani/progettou5w2/Progettou5w2ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescoandrisani.progettou5w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettou5w2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
